d0b678331897d06abf409ae77d8069c6b41fefdc
Smashinfries <jus10luka@gmail.com>
2024-08-04 19:30:34 -0500

    add supported links button

--- src/app/(tabs)/more/settings/index.tsx
+++ src/app/(tabs)/more/settings/index.tsx
@@ -1,11 +1,20 @@
 import { MaterialSwitchListItem } from '@/src/components/switches/switchListItem';
 import { StackView } from '@/src/components/view';
 import { useThemeStore } from '@/src/store/theme';
+import { ActivityAction, startActivityAsync } from 'expo-intent-launcher';
+import { List } from 'react-native-paper';
+import * as Linking from 'expo-linking';
 
 const SettingsPage = () => {
 	const { isDarkMode, isPureBlackMode, setIsDarkMode, setIsPureBlackMode } = useThemeStore();
 	return (
 		<StackView>
+			<List.Item
+				title={'Enable Supported Links'}
+				description={'Go to "Defaults" -> "Set as default"'}
+				right={(props) => <List.Icon {...props} icon={'open-in-new'} />}
+				onPress={() => Linking.openSettings()}
+			/>
 			<MaterialSwitchListItem
 				title="Dark Mode"
 				selected={isDarkMode}
